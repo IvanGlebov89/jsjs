8828c8461d8efcd85cc33233cc00adc804db690d
Ivan Glebov <125355249+IvanGlebov89@users.noreply.github.com>
2024-03-10 14:24:29 +0300

    testJs

--- app.js
+++ app.js
@@ -20,20 +20,46 @@ if (counters) {
     });
   });
 }
-const catalogHidden = document.querySelector(".catalog");
+const catalogProduct = document.querySelector(".catalog");
 const productBtn = document.querySelectorAll(".product-card__add-to-cart");
 const productCard = document.querySelectorAll(".product-card");
+const url = window.location.href;
+console.log(url);
+const ajax = fetch(url);
+ajax
+  .then((response) => {
+    return response.json;
+  })
+  .then((card) => {
+    console.log("card", card);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
+
 function openModal(elem) {
   elem.classList.add("_active");
+}
+function clearModal(elem) {
+  elem.classList.add("hide");
 }
 productBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let data = e.target.dataset.modelOpen;
     productCard.forEach((modal) => {
       if (modal.dataset.modal == data) {
-        console.log(modal);
         openModal(modal);
+        console.log(modal);
+      } else {
+        clearModal(modal);
       }
     });
   });
 });
+function catalogPush(elem, replace) {
+  const catalog = document.createElement("div");
+  catalog.className = "catalog";
+
+  catalog.push(elem);
+  replace.replaceWith(catalog);
+}
